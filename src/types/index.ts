7a5023d1ebbaa191751b598950336b984a27b194
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2025-02-25 22:08:30 +0300

    feat: added types

---
+++ src/types/index.ts
@@ -0,0 +1,47 @@
+// Интерфейс товара
+export interface IProductItem {
+  id: string;
+  description: string;
+  title: string;
+  category: string;
+  image: string;
+  price: number | null; // Товар может быть бесценным
+}
+
+// Интерфейс для корзины с товарами
+export interface IBasket {
+  items: IProductItem[];
+  total: number;
+}
+
+// Интефейс для формы заказа
+export interface IOrderForm {
+  payment: string;
+  address: string;
+  email: string;
+  phone: string;
+  validateDelivery(): boolean;
+  validateContacts(): boolean;
+}
+
+// Интефейс самого заказа
+export interface IOrder {
+  total: number;
+  items: IProductItem[];
+}
+
+// Интерфейс результата заказа, который отправляет сервер в результате post запроса
+export interface IOrderResult {
+  id: string;
+  total: number;
+}
+
+// Интерфейс для обработки действий
+export interface IAction {
+  onClick(event?: MouseEvent): void;
+} 
+
+// Интерфейс для обработки отображения
+export interface IView {
+  render(data?: object): HTMLElement;
+}
